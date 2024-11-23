0469048b10ce2432e9f9886eb2300d1611536294
Sajadh <officialsajadh@gmai.com>
2024-11-23 14:03:21 +0530

    whatsapp number updated

--- src/pages/whatsapp_button/WhatsAppButton.js
+++ src/pages/whatsapp_button/WhatsAppButton.js
@@ -5,7 +5,7 @@ import WhatsAppIcon from '@mui/icons-material/WhatsApp'; // Import the WhatsApp 
 const WhatsAppButton = () => {
   const handleClick = () => {
     // Open WhatsApp chat link (Add your WhatsApp number below)
-    window.open('https://wa.me/1234567890', '_blank');
+    window.open('https://wa.me/09744112113', '_blank');
   };
 
   return (
